c5ef7b6b4f27d04e5584d0cbf9f6c3927f6b4b4c
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-07-15 18:15:35 -0300

    Feito repositorio base em orm

--- eAgenda.Infraestrutura.Orm/ModuloContato/RepositorioContatoEmOrm.cs
+++ eAgenda.Infraestrutura.Orm/ModuloContato/RepositorioContatoEmOrm.cs
@@ -5,58 +5,10 @@ using eAgenda.Infraestrutura.Orm.Compartilhado;
 
 namespace eAgenda.Infraestrutura.Orm.ModuloContato
 {
-    public class RepositorioContatoEmOrm : IRepositorioContato
+    public class RepositorioContatoEmOrm : RepositorioBaseEmOrm<Contato>, IRepositorioContato
     {
-
-        private readonly eAgendaDbContext contexto;
-
-        public RepositorioContatoEmOrm(eAgendaDbContext contexto)
+        public RepositorioContatoEmOrm(eAgendaDbContext contexto) : base(contexto)
         {
-            this.contexto = contexto;
-        }
-        public void CadastrarRegistro(Contato novoRegistro)
-        {
-            contexto.Contatos.Add(novoRegistro);
-            contexto.SaveChanges();
-        }
-
-        public bool EditarRegistro(Guid idRegistro, Contato registroEditado)
-        {
-            var registroSelecionado = SelecionarRegistroPorId(idRegistro);
-
-            if(registroSelecionado is null)
-                return false;
-
-            else
-            {
-                registroSelecionado.AtualizarRegistro(registroEditado);
-                return true;
-            }
-            
-        }
-        public bool ExcluirRegistro(Guid idRegistro)
-        {
-            var registroSelecionado = SelecionarRegistroPorId(idRegistro);
-
-            if (registroSelecionado is null)
-                return false;
-
-
-            else
-            {
-                contexto.Contatos.Remove(registroSelecionado);
-                return true;
-            }
-        }
-
-        public Contato? SelecionarRegistroPorId(Guid idRegistro)
-        {
-            return contexto.Contatos.FirstOrDefault(x => x.Id.Equals(idRegistro));
-        }
-
-        public List<Contato> SelecionarRegistros()
-        {
-            return contexto.Contatos.ToList();
         }
     }
 }
